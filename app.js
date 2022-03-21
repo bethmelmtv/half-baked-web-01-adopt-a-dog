6e8790cafbe4a4caa00e195c516235881002c2f5
Beth <bethmelmtv@gmail.com>
2022-03-21 15:41:05 -0700

    worked on details.js function

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // fetch all dogs
 // render and append all dog cards to the container
+
+//why are we calling this window?
+window.addEventListener('load', async() => {
+
+    const Dogs = await getDogs();
+
+    for (let dog of Dogs) {
+        const dogEl = renderDogCard(dog);
+
+        dogListContainer.append(dogEl);
+        
+    }
+
+});
